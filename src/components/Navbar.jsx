bf167f90d2fe3780356fa96aae9273cfcb92806b
Loon <looncreates@gmail.com>
2026-04-13 11:20:40 -0400

    Finished initial coding for nav bar, moving on to hero component. Will probably revisit each component for some touching up in the end.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,8 @@
+import { Menu, XIcon } from "lucide-react";
+import { useState } from "react";
+
 export default function Navbar() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return(
      <nav className="fixed top-0 w-full z-50 transition-all duration-300 bg-slate-950/20 backdrop-blur-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -14,13 +18,27 @@ export default function Navbar() {
           </span>
 
         {/* Nav Links */}
-        <div className="flex item-center space-x-6 lg:space-x-8">
+        <div className="hidden md:flex item-center space-x-6 lg:space-x-8">
           <a href="#features" className="text-gray-300 hover:text-white text-sm lg:text-base">Features</a>
           <a href="#pricing" className="text-gray-300 hover:text-white text-sm lg:text-base">Pricing</a>
           <a href="#testimonials" className="text-gray-300 hover:text-white text-sm lg:text-base">Testimonials</a>
         </div>
+        <button className="md:hidden p-2 text-gray-300 hover:text-white" onClick={() => setMobileMenuOpen((prev) => !prev )}>
+          {
+          mobileMenuOpen ? (<XIcon />) : (<Menu className="w-5 h-5 sm:w-6 sm:h-6"/>)
+          }
+        </button>
         </div>
       </div>
+
+      {mobileMenuOpen && (
+        <div className="md:hidden bg-slate-900/95 backdrop-blur-lg border-t border-slate-800 animate-in slideInFromTop duration-300">
+          <div className="px-4 py-4 sm:py-6 space-y-3 sm:space-y-4">
+            <a href="#features" className="block text-gray-300 hover:text-white text-sm lg:text-base" onClick={() => setMobileMenuOpen(false)}>Features</a>
+            <a href="#pricing" className="block text-gray-300 hover:text-white text-sm lg:text-base " onClick={() => setMobileMenuOpen(false)}>Pricing</a>
+            <a href="#testimonials" className="block text-gray-300 hover:text-white text-sm lg:text-base" onClick={() => setMobileMenuOpen(false)}>Testimonials</a>
+          </div>
+        </div>)}
     </nav>
   );
 }
